e02a646c94bf087b03cc2ee1a8b8c00139d651e8
Ben Weschler <benjaminweschler@gmail.com>
2022-05-21 21:36:27 -0700

    Added complete HW 1 Skeleton.

--- lib/hw_1_skeleton.dart
+++ lib/hw_1_skeleton.dart
@@ -29,4 +29,89 @@ class LayoutOne extends StatelessWidget {
   }
 }
 
-/* ADD THE REST OF YOUR LAYOUTS HERE */
+class LayoutTwo extends StatelessWidget {
+  const LayoutTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class LayoutThree extends StatelessWidget {
+  const LayoutThree({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class LayoutFour extends StatelessWidget {
+  const LayoutFour({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class LayoutFive extends StatelessWidget {
+  const LayoutFive({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+/* HELPER WIDETS AND VARIABLES START BELOW THIS COMMENT */
+
+class ColoredSquare extends StatelessWidget {
+  final double side;
+  final Color color;
+
+  const ColoredSquare({
+    Key? key,
+    required this.side,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: side, height: side, color: color);
+  }
+}
+
+class ColoredArea extends StatelessWidget {
+  final Color color;
+
+  const ColoredArea(this.color, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: color);
+  }
+}
+
+class VPad extends StatelessWidget {
+  const VPad({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(height: 15);
+  }
+}
+
+class HPad extends StatelessWidget {
+  const HPad({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(width: 15);
+  }
+}
+
+const String loremIpsum =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
